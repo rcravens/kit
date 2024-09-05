0feb2683bcdead7c6b2adf82170cc759a4e6d862
Yevhenii Volosiuk <volosiukweb@gmail.com>
2024-09-05 02:38:08 +0200

    refactor: styling with quotes and sh rules

--- .code/bin/helpers/functions.sh
+++ .code/bin/helpers/functions.sh
@@ -11,8 +11,8 @@ command_exists() {
 # e.g., ensure_string_in_file "alias kit=\"./kit\"" ~/.bashrc
 #
 ensure_string_in_file() {
-	STRING=$1
-	FILE=$2
+	STRING="$1"
+	FILE="$2"
 	#echo "STRING: ${STRING}, FILE: ${FILE}"
 	if ! grep -qF "${STRING}" "${FILE}"; then
 		echo -e "${STRING}" >> "${FILE}"
@@ -26,16 +26,16 @@ ensure_string_in_file() {
 # will either update the value or add a line to the end of the file
 # e.g., set_value_in_file "DB_HOST" "localhost" ".env"
 set_value_in_file() {
-	VARIABLE=$1
+	VARIABLE="$1"
 	NEW_VALUE="$2"
-	FILE=$3
+	FILE="$3"
 	#echo "VARIABLE: ${VARIABLE}, NEW_VALUE: ${NEW_VALUE}, FILE: ${FILE}"
 	if ! grep -qF "${VARIABLE}=" "${FILE}"; then
 		#echo "variable not found in file...append it to the end"
-		echo -e "${VARIABLE}=${NEW_VALUE}" >> ${FILE}
+		echo -e "${VARIABLE}=${NEW_VALUE}" >> "${FILE}"
 	else
 		#echo "variable was found....update the value"
-		sed -i '' -e "s|${VARIABLE}=.*|${VARIABLE}=${NEW_VALUE}|" ${FILE}
+		sed -i '' -e "s|${VARIABLE}=.*|${VARIABLE}=${NEW_VALUE}|" "${FILE}"
 	fi
 }
 
@@ -78,9 +78,9 @@ bash_file() {
 # e.g., create_bash_alias kit "./kit"
 #
 create_bash_alias() {
-	ALIAS=$1
-	COMMAND=$2
-	STRING="alias ${ALIAS}=\"${COMMAND}\""
+	local ALIAS="$1"
+	local COMMAND="$2"
+	local STRING="alias ${ALIAS}=\"${COMMAND}\""
 
   BASH_FILE=$(bash_file)
 
@@ -91,12 +91,13 @@ create_bash_alias() {
 
 	if set_value_in_file "alias $ALIAS" "${COMMAND}" "${BASH_FILE}"; then
 		# file was modified...source it
-		source ${BASH_FILE}
+		# shellcheck source=/dev/null
+		source "${BASH_FILE}"
 	fi
 }
 
 capitalize() {
-  STR=$1
-  CAP_STR="$(tr '[:lower:]' '[:upper:]' <<< ${STR:0:1})${STR:1}"
+  local STR="$1"
+  CAP_STR="$(tr '[:lower:]' '[:upper:]' <<< "${STR:0:1}")${STR:1}"
   echo "${CAP_STR}"
 }

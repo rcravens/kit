67d54d2dbfc385f3c1a864769f33ed4c67880cba
rcravens <bob.cravens@gmail.com>
2024-09-06 16:49:00 -0500

    converted django to new .env.template file

--- .code/templates/django/bin/init.sh
+++ .code/templates/django/bin/init.sh
@@ -1,34 +1,15 @@
 #!/usr/bin/env bash
 
-# CODE_REPO_URL
-read -p "${GREEN}Enter the Git repository URL [$TEMPLATE_TYPE default]: ${RESET}" CODE_REPO_URL
-export CODE_REPO_URL
+# For each of the existing environments, use the .env.template file
+# as a template to create a new environment file with the correct environment variables.
+ENV_DIRECTORY="${APP_DIRECTORY}/envs/*"
+for DIR in ${ENV_DIRECTORY}; do
+  env="${DIR##*/}"
 
-# HTTP_ON_HOST
-read -p "${GREEN}Enter the external port [default: 8000]: ${RESET}" HTTP_ON_HOST
-HTTP_ON_HOST=${HTTP_ON_HOST:-8000}
-export HTTP_ON_HOST
+  echo_yellow "Updating environment: ${env}"
 
-
-# Initialize a new Laravel application
-#echo "django/new_init.sh"
-#echo "APP_NAME: $APP_NAME"
-#echo "CODE_REPO_URL: $CODE_REPO_URL"
-#echo "APP_DIRECTORY: $APP_DIRECTORY"
-#echo "HTTP_ON_HOST: $HTTP_ON_HOST"
-
-
-# COPY .env.example to the default envs
-for env in "${ENVS[@]}"
-do
-  ENV_DIRECTORY="${APP_DIRECTORY}/envs/${env}"
-  mkdir -p "${ENV_DIRECTORY}"
+  ENV_TEMPLATE_FILE="${APP_DIRECTORY}/envs/.env.template"
   ENV_FILE="${APP_DIRECTORY}/envs/${env}/.env"
-  cp "${APP_DIRECTORY}/.env.example" "${ENV_FILE}"
-  sed -i.bak "s|APP_NAME=.*|APP_NAME=${APP_NAME}|" "${ENV_FILE}"
-  sed -i.bak "s|APP_ENVIRONMENT=.*|APP_ENVIRONMENT=dev|" "${ENV_FILE}"
-  sed -i.bak "s|CODE_REPO_URL=.*|CODE_REPO_URL=${CODE_REPO_URL}|" "${ENV_FILE}"
-  sed -i.bak "s|HTTP_ON_HOST=.*|HTTP_ON_HOST=${HTTP_ON_HOST}|" "${ENV_FILE}"
-  rm "${ENV_FILE}.bak"
 
+  update_env_using_template "$ENV_TEMPLATE_FILE" "$ENV_FILE"
 done

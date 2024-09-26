e569890350b18daefe52c43d45dfd2327e1e6c00
rcravens <bob.cravens@gmail.com>
2024-09-26 09:05:02 -0500

    wip kit ssh <server> now creates ssh connection to swarm manager node

--- .code/bin/commands/ssh.sh
+++ .code/bin/commands/ssh.sh
@@ -8,9 +8,26 @@ function command_run {
     fi
 
     if [ -z "${ENTRY_SERVICE}" ]; then
-      echo "xxx"
-      echo "ENTRY_SERVICE: ${ENTRY_SERVICE}"
-      echo "ARGS: ${ARGS}"
+      SERVER="$1"
+      INVENTORY_FILE="$SERVERS_DIRECTORY/$SERVER/inventory.yml"
+#      echo $(parse_yaml "$INVENTORY_FILE")
+      eval $(parse_yaml "$INVENTORY_FILE")
+      JUMP_BOX_IP="${all_vars_jumpbox_host}"
+      JUMP_BOX_USER="${all_vars_jumpbox_user}"
+      JUMP_BOX_IDENT="${all_vars_jumpbox_key_file}"
+      JUMP_BOX_IDENT=$(echo "$JUMP_BOX_IDENT" | sed -r "s|~|${SERVERS_DIRECTORY}/${SERVER}|")
+      DESTINATION_BOX_IP="${swarm_manager_hosts___ansible_private_ip}"
+      DESTINATION_BOX_USER="${swarm_manager_vars_ansible_user}"
+      DESTINATION_BOX_IDENT="${swarm_manager_vars_ansible_private_key_file}"
+      DESTINATION_BOX_IDENT=$(echo "$DESTINATION_BOX_IDENT" | sed -r "s|~|${SERVERS_DIRECTORY}/${SERVER}|")
+#      echo "JUMP_BOX_IP: ${JUMP_BOX_IP}"
+#      echo "JUMP_BOX_USER: ${JUMP_BOX_USER}"
+#      echo "JUMP_BOX_IDENT: ${JUMP_BOX_IDENT}"
+#      echo "DESTINATION_BOX_IP: ${DESTINATION_BOX_IP}"
+#      echo "DESTINATION_BOX_USER: ${DESTINATION_BOX_USER}"
+#      echo "DESTINATION_BOX_IDENT: ${DESTINATION_BOX_IDENT}"
+
+      ssh -o ProxyCommand="ssh -W %h:%p -i ${JUMP_BOX_IDENT} ${JUMP_BOX_USER}@${JUMP_BOX_IP}" -i "${DESTINATION_BOX_IDENT}" "${DESTINATION_BOX_USER}@${DESTINATION_BOX_IP}"
     else
       run_docker_compose  exec -it "${ENTRY_SERVICE}" /bin/sh
     fi
